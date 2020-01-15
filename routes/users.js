a33467cb318871fa5f1fd2879c55f84ce2a7340b
Vincent Robles <sylkisgsx@gmail.com>
2020-01-14 22:20:00 -0500

    added index route

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const usersCtrl = require('../controllers/users');
 // GET /users/new
 router.get('/new', usersCtrl.new);
 router.post('/', usersCtrl.create);
+router.get('/', usersCtrl.index);
 
 module.exports = router;
