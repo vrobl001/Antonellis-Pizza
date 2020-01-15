e3cd61eb651bb6dd34a02b9c335a84f6df2461fe
Vincent Robles <sylkisgsx@gmail.com>
2020-01-14 20:40:15 -0500

    set up boilerplate and basic file structure

---
+++ server.js
@@ -0,0 +1,27 @@
+// Require Modules
+const express = require('express');
+const port = 3000;
+const logger = require('morgan');
+
+// Set up express app
+const app = express();
+
+// Connect to DB
+
+
+// Configure Express App app.set()
+app.set('view engine', 'ejs');
+
+// Mount middleware app.use()
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Mount Routes app.use()
+
+
+// Tell app to listen
+app.listen(port, () => {
+    console.log(`Express is listening on port:${port}`);
+});
