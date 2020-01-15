89fa7187267233cb0124b2c1cbda4e20de0f1768
Vincent Robles <sylkisgsx@gmail.com>
2020-01-14 21:53:08 -0500

    added create function

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,17 @@
+const User = require('../models/user');
+
 module.exports = {
-    new: newUser
+    new: newUser,
+    create
+}
+
+function create(req, res) {
+    const user = new User(req.body);
+    user.save(function(err) {
+        if(err) return res.render('users/new');
+        console.log(user);
+        res.redirect('/users/new');
+    });
 }
 
 function newUser(req, res) {
