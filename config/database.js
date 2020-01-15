d633ccfe0303a28ec1443dd30cc6781af260f425
Vincent Robles <sylkisgsx@gmail.com>
2020-01-14 20:58:34 -0500

    added event listeners to mongoose connection

--- config/database.js
+++ config/database.js
@@ -5,3 +5,10 @@ mongoose.connect('mongodb://localhost/userInfo', {
     useCreateIndex: true,
     useUnifiedTopology: true
 });
+
+// shortcut to mongoose.connection object
+const db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
