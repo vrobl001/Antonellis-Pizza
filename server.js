433fbaaa5dd12c111c62f0d09a6bac40018b89cb
Vincent Robles <sylkisgsx@gmail.com>
2020-01-14 20:47:58 -0500

    required and mounted index router

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const port = 3000;
 const logger = require('morgan');
+const indexRouter = require('./routes/index');
 
 // Set up express app
 const app = express();
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Mount Routes app.use()
-
+app.use('/', indexRouter);
 
 // Tell app to listen
 app.listen(port, () => {
