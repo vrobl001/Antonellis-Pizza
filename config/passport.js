c2532e92926c8500bcef1f1d0cf2c4ad89f8e2fa
Vincent Robles <sylkisgsx@gmail.com>
2020-01-15 19:36:04 -0500

    added callback

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -7,6 +8,21 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK,
 },
 function(accessToken, refreshToken, profile, cb) {
-
+    User.findOne({ 'googleId': profile.id }, function(err, user) {
+        if (err) return cb(err);
+        if (user) {
+          return cb(null, user);
+        } else {
+          const newUser = new User({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id
+          });
+          newUser.save(function(err) {
+            if (err) return cb(err);
+            return cb(null, newUser);
+        });
+    }
+  });
 }
 ));
