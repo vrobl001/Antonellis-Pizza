dc8eb32dfa5e4152c8f42ce75ae14076f594025a
Vincent Robles <sylkisgsx@gmail.com>
2020-01-15 19:51:30 -0500

    added google oauth callback and logout route

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,20 @@ router.get('/auth/google', passport.authenticate(
     'google',
     { scope: ['profile', 'email', 'https://www.googleapis.com/auth/user.phonenumbers.read'] }
   ));
+ 
+// Google OAuth callback route  
+router.get('/oauth2callback', passport.authenticate(
+'google',
+{
+    successRedirect : '/users',
+    failureRedirect : '/'
+}
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
 
 module.exports = router;
