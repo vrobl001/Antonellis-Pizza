fb7e1c35ab1cd0f25acaaab0ef98a4481b41a026
Vincent Robles <sylkisgsx@gmail.com>
2020-01-15 21:00:15 -0500

    added "add fact" for later use

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,22 @@ const User = require('../models/user');
 module.exports = {
     new: newUser,
     create,
-    index
-}
+    index,
+    addFact,
+};
+
+function addFact(req, res) {
+    req.user.facts.push(req.body);
+    req.user.save(function(err) {
+      res.redirect('/users');
+    });
+  }
 
 function index(req, res) {
     User.find({}, function(err, users) {
         res.render('users/index', {
-            users
+            users,
+            user: req.user
         });
     });
 }
