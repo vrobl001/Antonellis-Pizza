ecca2994671f42214184b3f368b547c35e42c0ee
Vincent Robles <sylkisgsx@gmail.com>
2020-01-15 19:40:41 -0500

    added serialize and deserialize

--- config/passport.js
+++ config/passport.js
@@ -26,3 +26,13 @@ function(accessToken, refreshToken, profile, cb) {
   });
 }
 ));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
