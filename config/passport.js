5b3ba70b597413b1e2ff80f347871f5cd4c9edfd
Vincent Robles <sylkisgsx@gmail.com>
2020-01-15 20:59:48 -0500

    passport shows profile pic

--- config/passport.js
+++ config/passport.js
@@ -3,25 +3,27 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
 },
 function(accessToken, refreshToken, profile, cb) {
-    User.findOne({ 'googleId': profile.id }, function(err, user) {
+  User.findOne({ 'googleId': profile.id }, function(err, user) {
+    console.log(profile)
+    if(err) return cb(err);
+    if(user) {
+      return cb(null, user);
+    } else {
+      const newUser = new User({
+        name: profile.displayName,
+        email: profile.emails[0].value,
+        avatarURL: profile.photos[0].value,
+        googleId: profile.id
+      });
+      newUser.save(function(err) {
         if (err) return cb(err);
-        if (user) {
-          return cb(null, user);
-        } else {
-          const newUser = new User({
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            googleId: profile.id
-          });
-          newUser.save(function(err) {
-            if (err) return cb(err);
-            return cb(null, newUser);
-        });
+        return cb(null, newUser);
+      });
     }
   });
 }
