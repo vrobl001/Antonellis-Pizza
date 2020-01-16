ea81a53b9daa9475302d217046b7fdd5745ec87b
Vincent Robles <sylkisgsx@gmail.com>
2020-01-15 20:01:56 -0500

    updated index action and passed in req.user

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,12 @@ module.exports = {
     index
 }
 
-function index(req, res) {
-    res.render('index');
+function index(req, res, next) {
+    User.find({}, function(err, users) {
+     res.render('users/index', {
+      users,
+      user: req.user
+      });
+   })
 }
+  
