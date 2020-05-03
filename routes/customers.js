dd0aacea843b8a2edbf00f06cb03b17b767bcb51
Vincent Robles <sylkisgsx@gmail.com>
2020-05-03 18:41:09 -0400

    organized routes

--- routes/customers.js
+++ routes/customers.js
@@ -7,19 +7,19 @@ router.get('/', customersCtrl.index);
 router.get('/new', customersCtrl.new);
 router.post('/customers', customersCtrl.create);
 router.get('/customers', isLoggedIn, customersCtrl.index);
-router.put('/:id', customersCtrl.update);
-router.get('/:id/edit',  customersCtrl.edit); //isLoggedIn
 
 // Update
+router.get('/:id/edit', customersCtrl.edit); //isLoggedIn
+router.put('/:id', customersCtrl.update);
 
 // Delete
 router.delete('/:id', (req, res) => {
-  res.send('Delete Customer Info ' + req.params.id)
-})
+  res.send('Delete Customer Info ' + req.params.id);
+});
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect('/auth/google');
-  }
+  if (req.isAuthenticated()) return next();
+  res.redirect('/auth/google');
+}
 
 module.exports = router;
