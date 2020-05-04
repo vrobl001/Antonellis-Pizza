48f87db320a5bf46a81e1c1460a6065e7e793461
Vincent Robles <sylkisgsx@gmail.com>
2020-05-03 21:48:43 -0400

    updated food model

--- models/food.js
+++ models/food.js
@@ -4,9 +4,33 @@ const Schema = mongoose.Schema;
 const foodSchema = new Schema(
   {
     name: String,
-    category: String,
-    price: String,
-    description: String,
+    category: {
+      type: String,
+      enum: [
+        'STARTERS & SIDES',
+        'WINGS',
+        'SALADS',
+        'PIZZA & TURNOVERS',
+        'PASTAS',
+        'WRAPS',
+        'SUBS',
+        'SANDWICHES',
+        'KIDS',
+        'DESSERTS',
+        'BEVERAGES',
+        'BEER & WINE',
+      ],
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    catering: Boolean,
   },
   {
     timestamps: true,
