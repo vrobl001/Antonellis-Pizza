c20f354f12284dfb41a8b3939ed14c9ca2e4d26e
Vincent Robles <sylkisgsx@gmail.com>
2020-05-04 09:34:40 -0400

    corrected routes

--- routes/customers.js
+++ routes/customers.js
@@ -3,23 +3,13 @@ const router = express.Router();
 const customersCtrl = require('../controllers/customer');
 
 // GET /customers/new
-router.get('/', customersCtrl.index);
-router.get('/new', customersCtrl.new);
-router.post('/customers', customersCtrl.create);
-router.get('/customers', isLoggedIn, customersCtrl.index);
+router.get('/', isLoggedIn, customersCtrl.index);
 router.post('/', customersCtrl.create);
-
+router.get('/new', customersCtrl.new);
+router.get('/:id/edit', isLoggedIn,  customersCtrl.edit); 
 
 // Update
 router.put('/:id', isLoggedIn, customersCtrl.update);
-router.get('/:id/edit', isLoggedIn,  customersCtrl.edit); 
-
-
-
-// Delete
-router.delete('/:id', (req, res) => {
-  res.send('Delete Customer Info ' + req.params.id);
-});
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
