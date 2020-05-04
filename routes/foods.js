c369a74859b1e0a97f8609aa8a43cbe3adabd27d
Vincent Robles <sylkisgsx@gmail.com>
2020-05-04 11:46:41 -0400

    protected routes

--- routes/foods.js
+++ routes/foods.js
@@ -3,15 +3,20 @@ const router = express.Router();
 const foodsCtrl = require('../controllers/food');
 
 // GET /foods/new
-router.get('/', foodsCtrl.index);
-router.post('/', foodsCtrl.create);
-router.get('/new', foodsCtrl.new);
-router.get('/:id/edit', foodsCtrl.edit);
+router.get('/', isLoggedIn, foodsCtrl.index);
+router.post('/', isLoggedIn, foodsCtrl.create);
+router.get('/new', isLoggedIn, foodsCtrl.new);
+router.get('/:id/edit', isLoggedIn, foodsCtrl.edit);
 
 // Update
 router.put('/:id', foodsCtrl.update);
 
 // Delete
 router.delete('/:id', foodsCtrl.deleteFood);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send('Not Authorized');
+  }
 
 module.exports = router;
